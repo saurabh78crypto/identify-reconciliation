2ce37c846e6ce8c7e1c78160464ddb3cd87074ed
Saurabh Pingale <saurabhpingale93@gmail.com>
2025-02-12 15:43:02 +0530

    feat: add contact identification logic

---
+++ controllers/contactController.js
@@ -0,0 +1,21 @@
+import db from "./config/db.js";
+
+// Identify and consolidate contact information
+exports.identifyContact = async (req, res) => {
+    const { email, phoneNumber } = req.body;
+
+    if(!email && !phoneNumber) {
+        return res.status(400).json({ error: "Either email or phone number is required"});
+    }
+
+    try {
+        // Find existing contact by email or phoneNumber
+        const [contacts] = await db.query(
+            `SELECT * FROM Contact WHERE email = ? OR phoneNumber = ?`,
+            [email, phoneNumber]
+        );
+    } catch (error) {
+        console.error(error);
+        return res.status(500).json({ error: "Internal Server Error"});
+    }
+}
