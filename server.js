367ee9665e34c277407e21750ebd2d3a9974dd0f
Saurabh Pingale <saurabhpingale93@gmail.com>
2025-02-12 16:34:22 +0530

    feat: add contactRoutes to server with /api endpoint

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ import express from "express";
 import bodyParser from "body-parser";
 import cors from "cors";
 import dotenv from "dotenv";
+import contactRoutes from "./routes/contactRoutes.js";
 
 dotenv.config();
 
@@ -9,7 +10,9 @@ const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
-const { PORT } = process.env || 5000;
+app.use("/api", contactRoutes);
+
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
     console.log(`Server running on ${PORT}`);
 });
